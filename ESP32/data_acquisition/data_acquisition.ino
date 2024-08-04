bdfcf1676c8462639ef415fac203495d1f3e3cef
Daniele Polidori <daniele.polidori2@studio.unibo.it>
2024-08-04 13:26:43 +0200

    [DA] coap observe (non funzionante)

--- ESP32/data_acquisition/data_acquisition.ino
+++ ESP32/data_acquisition/data_acquisition.ino
@@ -37,6 +37,10 @@ float tempValue;
 //bool resultMQTT;
 
 
+/* CoAP observe */
+Thing::CoAP::IFunctionalResource* risorsa_foo;   // Puntatore a una risorsa
+
+
 
 void connect() {
   
@@ -79,9 +83,8 @@ void setup() {
   //Configure our server to use our packet handler (It will use UDP)
   server.SetPacketProvider(udpProvider);
 
-  //Create an resource called "temperatura_interna"
-  //server.CreateResource("temperatura_interna", Thing::CoAP::ContentFormat::TextPlain, true)   // True means that this resource is observable
-  server.CreateResource("temperatura_interna", Thing::CoAP::ContentFormat::TextPlain, false)    //True means that this resource is observable
+  //Create a resource called "temperatura_interna"
+  *risorsa_foo = server.CreateResource("temperatura_interna", Thing::CoAP::ContentFormat::TextPlain, true)    //True means that this resource is observable
     .OnGet([](Thing::CoAP::Request & request) {                       // We are here configuring telling our server that, when we receive a "GET" request to this endpoint, run the the following code
 
       Serial.println("GET Request received for endpoint 'temperatura_interna'");
@@ -102,6 +105,25 @@ void setup() {
 
 
 void loop() {
-
+  
   server.Process();
+
+
+
+  /* CoAP observe */
+
+  delay(5000);
+  //delay(DEFAULT_SENSE_FREQUENCY);
+  
+  float value = dht_int.readTemperature();
+  String message = String(value);
+  const char* payload = message.c_str();
+  Serial.println(payload);
+
+  // DA AGGIUNGERE: if valore temperatura e' cambiato... (... notifico gli observer)
+  if (risorsa_foo != NULL) {
+    
+    //server.NotifyObservers(risorsa_foo, Thing::CoAP::Status::Content(payload));
+    risorsa_foo->ObservableChanged(Thing::CoAP::Status::Content(payload));
+  }
 }
--- python_script/main.py
+++ python_script/main.py
@@ -11,25 +11,43 @@ from aiocoap import *
 
 logging.basicConfig(level=logging.INFO)
 
+# CoAP observe
+def observation_callback(response):
+    print("callback: %r" % response.payload)
+
 async def main():
 
     protocol = await Context.create_client_context()
 
+    stop = False
     while(True):
 
-        #req = Message(code=GET, uri='coap://130.136.2.70:5683/temperatura_interna')
-        req = Message(code=GET, uri='coap://192.168.24.4:5683/temperatura_interna')
+        if stop==False:     # Codice eseguito una sola volta
 
-        try:
-            response = await protocol.request(req).response
-        except Exception as e:
-            print('Failed to fetch resource:')
-            print(e)
-        else:
-            # “2.05 Content” is a successful message (is the rough equivalent of HTTP’s “200 OK”)
-            print('Result: %s\n%r' % (response.code, response.payload.decode("utf-8")))
+            #req = Message(code=GET, uri='coap://192.168.24.4:5683/temperatura_interna')
+            req = Message(code=GET, uri='coap://192.168.104.4:5683/temperatura_interna')
 
-        time.sleep(7)
+            # OBSERVE
+            req.opt.observe = 0     # Set observe bit from None to 0
+
+            try:
+                #response = await protocol.request(req).response
+
+                # OBSERVE
+                # The message payload is printed whenever a new notification message is received from the server
+                protocol_request = protocol.request(req)
+                protocol_request.observation.register_callback(observation_callback)
+                response = await protocol_request.response
+            except Exception as e:
+                print('Failed to fetch resource:')
+                print(e)
+            else:
+                # “2.05 Content” is a successful message (is the rough equivalent of HTTP’s “200 OK”)
+                print('Result: %s\n%r' % (response.code, response.payload.decode("utf-8")))
+
+            #time.sleep(7)
+
+        stop = True
 
 if __name__ == "__main__":
-        asyncio.run(main())
+    asyncio.run(main())
